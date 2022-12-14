980a1f1d7af1e26cc2480847fe402cc822af3b0b
DESKTOP-QVOJ483\iibi203 <lojugt98@gmail.com>
2022-12-14 09:09:02 +0900

    ResultScene 수정

--- Assets/Scripts/Scene/ResultScene.cs
+++ Assets/Scripts/Scene/ResultScene.cs
@@ -7,12 +7,18 @@ public class ResultScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenResult();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OpenResult()
     {
-        
+        Object rs = FindObjectOfType<UIResult>();
+        if (rs == null)
+        {
+            Object rrs = Resources.Load("UI/UIResult");
+            GameObject result = (GameObject)Instantiate(rrs);
+            result.GetComponent<UIMain>();
+        }
     }
 }
